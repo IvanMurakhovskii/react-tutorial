a535b2961890840a61c6ac7a16bc1da68d23dc37
IvamMurakhovskii <ivan112247@gmail.ru>
2020-10-21 10:27:46 +0300

    feat: created function for clean code tasks

---
+++ src/homework_8/cleanFunction.ts
@@ -0,0 +1,52 @@
+// 1. Лучшая команда (наибольшее кол-во очков), выводим имя
+type Team = {
+    name: string,
+    score: number
+};
+
+const teams: Team[] = [
+    { name: 'Lions', score: 5 },
+    { name: 'Tigers', score: 4 },
+    { name: 'Bears', score: 6 },
+    { name: 'Monkeys', score: 2 },
+]
+
+const getTopName = (teams: Team[]): string => {
+    const result = teams.reduce((team, currentTeam) => {
+        if (team.score > currentTeam.score) return team;
+        return currentTeam;
+    }).name;
+
+    return result;
+}
+
+const topTeamName: string = getTopName(teams);
+console.log(`Top team name is ${topTeamName}`);
+
+// 2.Querystring из объекта
+
+const qsObj = {
+    page: "2",
+    pageSize: "10",
+    total: "205",
+    somethingElse: "value"
+}
+
+// '?page=2&amp;pageSize=10&amp;total=205&amp;somethingElse=value'
+const createQs = (qsObj: any) => {
+    return '?' + Object.keys(qsObj).map(k => {
+        return k + '=' + qsObj[k];
+    }).join('&');
+}
+const query = createQs(qsObj);
+console.log(query);
+
+// 3. Объект из querystring
+const qs = '?page=2&amp;pageSize=10&amp;total=205&amp;somethingElse=value'
+
+// { page: '2', pageSize: '10', total: '205', somethingElse: 'value' }
+const parseQs = (qs) => {
+
+}
+const result = parseQs(qs)
+console.log(result)
